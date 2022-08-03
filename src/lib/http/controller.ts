c9e638e03a2f5e2bc9716e56b74a14af4a758fbf
Alert175 <ilya290897@gmail.com>
2022-08-03 23:32:28 +0300

    config

--- src/lib/http/controller.ts
+++ src/lib/http/controller.ts
@@ -1,8 +1,11 @@
+import * as path from "fs"
+
 export class Controller {
 	private appPath: string
 
 	constructor(applicationPathPreifix?: string) {
-		this.appPath = applicationPathPreifix || ""
+		this.appPath = applicationPathPreifix || "";
+		console.log(__dirname)
 	}
 
 	
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
-// const http = require('http');
-import http from "http"
+import * as http from "http"
+
+import { Controller } from "./lib/http/controller";
+
 
 const requestListener = function (req: any, res: any) {
   res.writeHead(200);
@@ -8,5 +10,25 @@ const requestListener = function (req: any, res: any) {
 
 const server = http.createServer(requestListener);
 server.listen(5002, "0.0.0.0", () => {
-	console.log("App running on port 3000")
+	console.log("App running on port 303")
 });
+
+
+
+
+
+
+// Here HMR story begins
+declare const module: any;
+
+
+
+// You need only 3 lines of code to start accepting code changes coming through the HMR
+if (module.hot) {
+    module.hot.accept();
+    // Next callback is essential: After code changes were accepted     we need to restart the app. server.close() is here Express.JS-specific and can differ in other frameworks. The idea is that you should shut down your app here. Data/state saving between shutdown and new start is possible
+    module.hot.dispose(() => server.close());
+}
+
+
+const c = new Controller()
